2dc4f865c7aeb001f9f7858456c732b70c2e29a3
Rafael Martinelli <martinelli@puc-rio.br>
2021-04-27 22:10:17 -0300

    Closes #3.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,67 @@
 using CARPData
 using Test
 
-@testset "CARPData.jl" begin
-    # Write your tests here.
+@testset "kshs" begin
+    data = load(:kshs3)
+    @test data.lb == 9320
+    @test data.ub == 9320
+    @test data.name == "kshs3"
+    @test length(data.vertices) == 6
+    @test length(data.edges) == 15
+    @test length(data.requireds) == 15
+end
+
+@testset "gdb" begin
+    data = load(:gdb8)
+    @test data.lb == 348
+    @test data.ub == 348
+    @test data.name == "gdb8"
+    @test length(data.vertices) == 27
+    @test length(data.edges) == 46
+    @test length(data.requireds) == 46
+end
+
+@testset "val" begin
+    data = load(:val9B)
+    @test data.lb == 326
+    @test data.lb == 326
+    @test data.name == "val9B"
+    @test length(data.vertices) == 50
+    @test length(data.edges) == 92
+    @test length(data.requireds) == 92
+end
+
+@testset "egl" begin
+    data = load(:egl_s2_A)
+    @test data.lb == 9838
+    @test data.ub == 9875
+    @test data.name == "egl-s2-A"
+    @test length(data.vertices) == 140
+    @test length(data.edges) == 190
+    @test length(data.requireds) == 147
+end
+
+@testset "beullens" begin
+    data = load(:F16)
+    @test data.lb == 2725
+    @test data.ub == 2725
+    @test data.name == "F16"
+    @test length(data.vertices) == 60
+    @test length(data.edges) == 80
+    @test length(data.requireds) == 54
+end
+
+@testset "egl-large" begin
+    data = load(:egl_g1_E)
+    @test data.lb == 1461469
+    @test data.ub == 1506493
+    @test data.name == "egl-g1-E"
+    @test length(data.vertices) == 255
+    @test length(data.edges) == 375
+    @test length(data.requireds) == 347
+end
+
+@testset "Load string" begin
+    data = load("CARPData/data/kshs1.dat")
+    @test data !== nothing
 end
